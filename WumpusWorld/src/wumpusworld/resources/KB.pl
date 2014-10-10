dc9f3be8a9e8de4cfefea69767c7a69336ccd01e
Smurfa <smurfa-021@hotmail.com>
2014-10-10 13:28:06 +0200

    fdsaf

--- WumpusWorld/src/wumpusworld/resources/KB.pl
+++ WumpusWorld/src/wumpusworld/resources/KB.pl
@@ -14,6 +14,7 @@ location_toward([X,Y],0,[X,New_Y]) :- New_Y is Y+1.
 location_toward([X,Y],3,[New_X,Y]) :- New_X is X-1.
 location_toward([X,Y],2,[X,New_Y]) :- New_Y is Y-1.
 
+adjacent(X,Y) :- location_toward(X,_,Y).
 
 %--------------------------------
 %Misc rules
@@ -24,36 +25,58 @@ location_toward([X,Y],2,[X,New_Y]) :- New_Y is Y-1.
 perception(X) :- breeze(X).
 perception(X) :- stench(X).
 
+
+
+
+
 %--------------------------------
 %Handle wumpus logic
 %--------------------------------
+stench(X) :- wumpus_pos(Z), adjacent(X,Z).
+
+
+
+
 wumpus(X) :- not(visited(X)), location_toward(X,_,Z), stench(Z), inside_bounds(X), inside_bounds(Z).
+
+is_wumpus(X) :- not(visited(X)), adjacent(X,Y), stench(Y).
 
 add_visited(X) :- asserta(visited(X)), retract(assume_wumpus(X)).
 
 %add_stench(X) :- assert(stench(X)), check_for_wumpus(X,0), check_for_wumpus(X,1), check_for_wumpus(X,2), check_for_wumpus(X,3).
-add_stench(X) :- assert(stench(X)), check_for_wumpus(X,0), check_for_wumpus(X,1), check_for_wumpus(X,2), check_for_wumpus(X,3), !.
+add_stench(X) :- asserta(stench(X)), ignore(check_for_wumpus(X,0)), ignore(check_for_wumpus(X,1)), ignore(check_for_wumpus(X,2)), ignore(check_for_wumpus(X,3)), !.
 
-check_for_wumpus(X,D) :- wumpus_found -> !,fail; location_toward(X,D,Y), not(visited(Y)), inside_bounds(Y),
-    (assume_wumpus(Y) -> (assert(wumpus_found), abolish(assume_wumpus(_))); asserta(assume_wumpus(Y))).
+check_for_wumpus(X,D) :- (
+						location_toward(X,D,Y), not(visited(Y)), inside_bounds(Y),
+    							(
+    							assume_wumpus(Y) -> (retractall(assume_wumpus(X_)), asserta(wumpus_pos(Y))); asserta(assume_wumpus(Y))
+    							)
+    						).
 %(assume_wumpus(Y) -> (assert(wumpus_found), abolish(assume_wumpus(_)), assert(wumpus_pos(Y))); 
 
 
 %--------------------------------
 %Handle pit logic
 %--------------------------------
-breeze(X) :- location_toward(X,_,Y), pit(Y).
+breeze(X) :- pit(Z), adjacent(X,Z).
 
 %--------------------------------
 %Dynamic variables
 %--------------------------------
-:- dynamic(assume_wumpus/1).
-:- dynamic(stench/1).
-:- dynamic(wumpus_found/0).
-:- dynamic(wumpus_pos/1).
-:- dynamic(visited/1).
-:- dynamic(breeze/1).
-
+%:- dynamic(assume_wumpus/1).
+%:- dynamic(stench/1).
+%:- dynamic(wumpus_found/0).
+%:- dynamic(wumpus_pos/1).
+%:- dynamic(visited/1).
+%:- dynamic(breeze/1).
+:- dynamic([
+            assume_wumpus/1,
+            stench/1,
+            wumpus_found/0,
+            wumpus_pos/1,
+            visited/1,
+            breeze/1
+        ]).
 
 %--------------------------------
 %Default values
@@ -67,17 +90,20 @@ assume_wumpus(_) :- fail.
 %Set functions
 %--------------------------------
 add_breeze(X) :- (inside_bounds(X), breeze(X)) ; asserta(breeze(X)).
+%add_stench(X) :- (inside_bounds(X), stench(X)) ; asserta(stench(X)).
+
 add_glitter(X) :- (inside_bounds(X), glitter(X)) ; asserta(glitter(X)).
-add_visited(X) :- (inside_bounds(X), visited(X)) ; asserta(visited(X)).
+add_visited(X) :- (inside_bounds(X), visited(X)) ; (asserta(visited(X)), retract(assume_wumpus(X))).
 add_pit(X) :- (inside_bounds(X), pit(X)) ; asserta(pit(X)).
 
 %--------------------------------
 %Set not functions
 %--------------------------------
-add_not_breeze(X) :- (inside_bounds(X), not(breeze(X))) ; asserta(not(breeze(X))).
-add_not_glitter(X) :- (inside_bounds(X), not(glitter(X))) ; asserta(not(glitter(X))).
-add_not_visited(X) :- (inside_bounds(X), not(visited(X))) ; asserta(not(visited(X))).
-add_not_pit(X) :- (inside_bounds(X), not(pit(X))) ; asserta(not(pit(X))).
+%add_not_breeze(X) :- (inside_bounds(X), not(breeze(X))) ; asserta(not(breeze(X))).
+%add_not_stench(X) :- (inside_bounds(X), not(stench(X))) ; asserta(not(stench(X))).
+%add_not_glitter(X) :- (inside_bounds(X), not(glitter(X))) ; asserta(not(glitter(X))).
+%add_not_visited(X) :- (inside_bounds(X), not(visited(X))) ; asserta(not(visited(X))).
+%add_not_pit(X) :- (inside_bounds(X), not(pit(X))) ; asserta(not(pit(X))).
 
 
 %--------------------------------
